d2c6149fee6d55b55fcb577613c82e3a4fef5179
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-30 19:58:37 +0800

    Delete resize_img.py

--- resize_img.py
+++
@@ -1,8 +0,0 @@
-import os
-import cv2 as cv
-for file in os.listdir('output'):
-    path = os.path.join('output', file)
-    img = cv.imread(path)
-    print(img.shape)
-    img = cv.resize(img, (416, 416))
-    cv.imwrite(path, img)
